579f2e996f3808326c5ce4e49c76602a3f697f76
LINXIAOXIAO <lin123456steve@outlook.com>
2021-09-18 21:02:23 +0800

    initialize c_algorithm practice

---
+++ c_algorithm_array/1contains duplicate.c
@@ -0,0 +1,8 @@
+// 问题描述：Given an integer array nums, return true if 
+//any value appears at least twice in the array, and return 
+//false if every element is distinct.
+
+// Constraints:
+// 1 <= nums.length <= 10^5
+// -10^9 <= num[i] <= 10^9
+
