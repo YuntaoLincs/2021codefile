273afab43673c99dba142a4cacde7be5b05e11e7
LINXIAOXIAO <lin123456steve@outlook.com>
2021-09-20 00:12:47 +0800

    add a new file

---
+++ Comp9024_workfile/quiz1.c
@@ -0,0 +1,6 @@
+// Questions' descrition
+//Write a C-program that outputs, in alphabetical order, all strings that 
+//use each of the characters 'c', 'a', 't', 'd', 'o', 'g' exactly once.
+//How many strings does your program generate?
+
+
